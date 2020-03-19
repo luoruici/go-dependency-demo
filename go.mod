930c40b5e9dcd168816824bd64acaba5b1dae73b
Ruici Luo <luoruici@bytedance.com>
2020-03-19 18:38:11 +0800

    testcase.
    Change-Id: I1652c7e8371e5b0282518cc63542b2314212375e

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.4.0
-	rsc.io/quote/v3 v3.0.0
+	rsc.io/quote/v3 v3.1.0
 )
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	s := Hello()
-	if s != "Hello, world." {
+	if s != "Hello, world.1" {
 		t.Errorf("error")
 	}
 }
